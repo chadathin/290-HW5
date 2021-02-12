a6bbf1a50f8a133cd2466c67b97b735b4d5ad8b4
Chad <chad.boehm@gmail.com>
2021-02-11 19:11:45 -0800

    Had to change variable name

--- getPost.js
+++ getPost.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 7321);
 
 app.get('/',function(req,res){
   res.render('home.handlebars') //We can omit the .handlebars extension as we do below
@@ -20,7 +20,7 @@ app.get('/',function(req,res){
 app.post('/sent', function(req,res){
   var queries = [];
   for (var q in req.body){
-    qParams.push({'name':q,'value':req.body[q]})
+    queries.push({'name':q,'value':req.body[q]})
   }
   //console.log(queries);
   //console.log(req.body);
